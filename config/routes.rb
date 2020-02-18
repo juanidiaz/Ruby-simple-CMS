39ea26cb9a47d9d1a7c911388551b08f20939c76
Juan I Diaz <jdi@idiaz.ca>
2020-02-17 23:07:20 -0500

    Testing simple, default and root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # ROOT ROUTE
+  # root 'demo#index'
+  # match "/", :to => "demo#index", :via => :get
+
+  # SIMPLE MATCH ROUTE
+  # get 'demo/index'
+  # match "here", :to => "demo#index", :via => :get
+
+  # DEFAULT ROUTE
+  # get ':controller(/:action(/:id))'
+  # match ':controller(/:action(/:id))', :via => :get
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
